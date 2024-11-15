b9fd1a93fba7233a8012d003192449f341c50a34
unknown <you@example.com>
2024-11-15 13:43:14 +0700

    server modifications

--- server/src/index.ts
+++ server/src/index.ts
@@ -37,7 +37,7 @@ app.use("/users", userRoutes)
 app.use("/teams", teamRoutes)
 
 // Server
-const PORT = process.env.PORT || 3000
-app.listen(PORT, () => {
+const PORT = Number(process.env.PORT) || 3000
+app.listen(PORT,"0.0.0.0", () => {
   console.log(`Server running on port ${PORT}`)
 })
